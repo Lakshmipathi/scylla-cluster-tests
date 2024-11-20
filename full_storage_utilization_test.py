15d6603941892daf81bd05fe06b110b29bfc33d3
Lakshmipathi <lakshmipathi@giis.co.in>
2024-11-20 11:42:23 +0530

    fix: add new utils
    Signed-off-by: Lakshmipathi <lakshmipathi@giis.co.in>

--- full_storage_utilization_test.py
+++ full_storage_utilization_test.py
@@ -1,7 +1,13 @@
 from enum import Enum
 import time
+from sdcm.sct_events import Severity
+from sdcm.sct_events.system import InfoEvent
 from sdcm.tester import ClusterTester
+from sdcm.utils.decorators import latency_calculator_decorator
 from sdcm.utils.tablets.common import wait_for_tablets_balanced
+from sdcm.utils.full_storage_utils import DiskUtils, StressUtils
+from sdcm import nemesis
+from sdcm import prometheus
 
 class ScalingActionType(Enum):
     SCALE_OUT = "scale_out"
@@ -19,24 +25,13 @@ class FullStorageUtilizationTest(ClusterTester):
         self.stress_cmd_r = self.params.get('stress_cmd_r')
         self.add_node_cnt = self.params.get('add_node_cnt')
         self.scaling_action_type = self.params.get('scaling_action_type')
-        self.total_large_ks=0
-        self.total_small_ks=0
-
-    def prepare_dataset_layout(self, dataset_size, row_size=10240):
-        n = dataset_size * 1024 * 1024 * 1024 // row_size
-        seq_end = n * 100
-        cores = self.db_cluster.nodes[0].cpu_cores
-        if not cores:
-           self.num_stress_threads = 10
-        else:
-           self.num_stress_threads = int(cores) * 8
-
-        return f'cassandra-stress write cl=ONE n={n} -mode cql3 native -rate threads={self.num_stress_threads} -pop dist="uniform(1..{seq_end})" ' \
-               f'-col "size=FIXED({row_size}) n=FIXED(1)" -schema "replication(strategy=NetworkTopologyStrategy,replication_factor=3)"'
+        self.stress_utils = None 
+        self.metrics_srv = prometheus.nemesis_metrics_obj()
 
     def setUp(self):
         super().setUp()
         self.start_time = time.time()
+        self.stress_utils = StressUtils(db_node=self.db_cluster.nodes[0], cluster_tester=self)
 
     def start_throttle_write(self):
         self.stress_cmd_w = self.stress_cmd_w.replace("<THREADS_PLACE_HOLDER>", str(self.num_stress_threads))
@@ -54,6 +49,7 @@ class FullStorageUtilizationTest(ClusterTester):
         time.sleep(self.sleep_time_before_scale)
 
     def start_throttle_rw(self):
+        InfoEvent(message="Starting throttled read write", severity=Severity.WARNING).publish()
         self.start_throttle_write()
         self.start_throttle_read()
         
@@ -83,7 +79,7 @@ class FullStorageUtilizationTest(ClusterTester):
         with self.db_cluster.cql_connection_patient(node) as session:
            session.execute(query)
            #node.run_nodetool(f"clearsnapshot")
-        self.log_disk_usage()     
+        DiskUtils.log_disk_usage(self.db_cluster.nodes)     
 
     def perform_scale_in(self):
         '''
@@ -106,17 +102,26 @@ class FullStorageUtilizationTest(ClusterTester):
            self.scale_in()
         elif self.hardlimit == 67:
            self.scale_in()
+        self.verify_stress_thread(cs_thread_pool=self.stress_cmd_r)
+        self.verify_stress_thread(cs_thread_pool=self.stress_cmd_w)
 
+    @latency_calculator_decorator(legend="Scale task")
     def perform_action(self):
-        self.log_disk_usage()
+        DiskUtils.log_disk_usage(self.db_cluster.nodes)
+        #self.db_cluster.add_nemesis(nemesis=self.get_nemesis_class(),
+        #                            tester_obj=self)
+        #self.db_cluster.start_nemesis()
         # Trigger specific action
+        self.metrics_srv.event_start('Starting scaling action')
+        InfoEvent(message=f"Starting {self.scaling_action_type}", severity=Severity.WARNING).publish()
         if self.scaling_action_type == ScalingActionType.SCALE_OUT.value:
            self.scale_out()
         elif self.scaling_action_type == ScalingActionType.SCALE_IN.value:
            self.perform_scale_in()
         else:
            self.log.info(f"Invalid ActionType {self.scaling_action_type}")
-        self.log_disk_usage()     
+        self.metrics_srv.event_stop('Starting scaling action')
+        DiskUtils.log_disk_usage(self.db_cluster.nodes)
 
     def test_storage_utilization(self):
         """
@@ -124,50 +129,27 @@ class FullStorageUtilizationTest(ClusterTester):
         Sleep for 60 minutes.
         Perform specific action.
         """
+        InfoEvent(message=f"Populate data until softlimit {self.softlimit} reached", severity=Severity.WARNING).publish()
+        self.metrics_srv.event_start('populate until softlimit')
         self.run_stress(self.softlimit, sleep_time=self.sleep_time_fill_disk)
+        self.metrics_srv.event_stop('populate until softlimit')
+        InfoEvent(message=f"Populate data until hardlimit {self.hardlimit} reached", severity=Severity.WARNING).publish()
+        self.metrics_srv.event_start('populate until hardlimit')
         self.run_stress(self.hardlimit, sleep_time=self.sleep_time_fill_disk)
+        self.metrics_srv.event_stop('populate until hardlimit')
         self.perform_action()        
+        self.log.info("Waiting for 1hr and check disk usage")
+        DiskUtils.log_disk_usage(self.db_cluster.nodes)
 
     def run_stress(self, target_usage, sleep_time=600):
-        target_used_size = self.calculate_target_used_size(target_usage)
-        self.run_stress_until_target(target_used_size, target_usage)
+        target_used_size = DiskUtils.calculate_target_used_size(
+            self.db_cluster.nodes, target_usage)
+        self.stress_utils.run_stress_until_target(target_used_size, target_usage)
 
-        self.log_disk_usage()
+        DiskUtils.log_disk_usage(self.db_cluster.nodes)
         self.log.info(f"Wait for {sleep_time} seconds")
         time.sleep(sleep_time)  
-        self.log_disk_usage()
-
-    def run_stress_until_target(self, target_used_size, target_usage):
-        current_usage, current_used = self.get_max_disk_usage()
-        smaller_dataset = False
-        
-        space_needed = target_used_size - current_used
-        # Calculate chunk size as 10% of space needed
-        chunk_size = int(space_needed * 0.1)
-        while current_used < target_used_size and current_usage < target_usage:
-            # Write smaller dataset near the threshold (15% or 30GB of the target)
-            smaller_dataset = (((target_used_size - current_used) < 30) or ((target_usage - current_usage) <= 15))
-            if not smaller_dataset:
-                self.total_large_ks += 1
-            else:
-                self.total_small_ks += 1
-
-            # Use 1GB chunks near threshold, otherwise use 10% of remaining space
-            dataset_size = 1 if smaller_dataset else chunk_size
-            ks_name = "keyspace_small" if smaller_dataset else "keyspace_large"
-            num = self.total_small_ks if smaller_dataset else self.total_large_ks
-            self.log.info(f"Writing chunk of size: {dataset_size} GB")
-            stress_cmd = self.prepare_dataset_layout(dataset_size)
-            stress_queue = self.run_stress_thread(stress_cmd=stress_cmd, keyspace_name=f"{ks_name}{num}", stress_num=1, keyspace_num=num)
-
-            self.verify_stress_thread(cs_thread_pool=stress_queue)
-            self.get_stress_results(queue=stress_queue)
-
-            self.db_cluster.flush_all_nodes()
-            #time.sleep(60) if smaller_dataset else time.sleep(600)
-
-            current_usage, current_used = self.get_max_disk_usage()
-            self.log.info(f"Current max disk usage after writing to keyspace{num}: {current_usage}% ({current_used} GB / {target_used_size} GB)")
+        DiskUtils.log_disk_usage(self.db_cluster.nodes)
 
     def add_new_node(self):
         new_nodes = self.db_cluster.add_nodes(count=self.add_node_cnt, enable_auto_bootstrap=True)
@@ -186,51 +168,3 @@ class FullStorageUtilizationTest(ClusterTester):
         self.log.info(f"Node {node_to_remove.name} has been removed from the cluster")
         self.monitors.reconfigure_scylla_monitoring()
         wait_for_tablets_balanced(self.db_cluster.nodes[0])
-    
-    def get_max_disk_usage(self):
-        max_usage = 0
-        max_used = 0
-        for node in self.db_cluster.nodes:
-            info = self.get_disk_info(node)
-            max_usage = max(max_usage, info["used_percent"])
-            max_used = max(max_used, info["used"])
-        return max_usage, max_used
-
-    def get_disk_info(self, node):
-        result = node.remoter.run("df -h -BG --output=size,used,avail,pcent /var/lib/scylla | sed 1d | sed 's/G//g' | sed 's/%//'")
-        size, used, avail, pcent = result.stdout.strip().split()
-        return {
-            'total': int(size),
-            'used': int(used),
-            'available': int(avail),
-            'used_percent': int(pcent)
-        }
-
-    def calculate_target_used_size(self, target_percent):
-        max_total = 0
-        for node in self.db_cluster.nodes:
-            info = self.get_disk_info(node)
-            max_total = max(max_total, info['total'])
-        
-        target_used_size = (target_percent / 100) * max_total
-        current_usage, current_used = self.get_max_disk_usage()
-        additional_usage_needed = target_used_size - current_used
-
-        self.log.info(f"Current max disk usage: {current_usage:.2f}%")
-        self.log.info(f"Current max used space: {current_used:.2f} GB")
-        self.log.info(f"Max total disk space: {max_total:.2f} GB")
-        self.log.info(f"Target used space to reach {target_percent}%: {target_used_size:.2f} GB")
-        self.log.info(f"Additional space to be used: {additional_usage_needed:.2f} GB")
-
-        return target_used_size
-
-    def log_disk_usage(self):
-        for node in self.db_cluster.nodes:
-            info = self.get_disk_info(node)
-            self.log.info(f"Disk usage for node {node.name}:")
-            self.log.info(f"  Total: {info['total']} GB")
-            self.log.info(f"  Used: {info['used']} GB")
-            self.log.info(f"  Available: {info['available']} GB")
-            self.log.info(f"  Used %: {info['used_percent']}%")
-
-    
